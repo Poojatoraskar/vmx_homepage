8584799e81963917ac5da38ed822f9528db72b8a
Your Name <pooja.toraskar@neosoftmail.com>
2022-08-23 17:00:37 +0530

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,7 @@ $('.owl_country').owlCarousel({
 })
 
 
-$('.owl_yos').owlCarousel({
+$('.owl-an-opreting-system').owlCarousel({
     loop:true,
     margin:10,
     responsiveClass:true,
@@ -37,46 +37,32 @@ $('.owl_yos').owlCarousel({
     }
 })
 
-
-
-function openCity(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+$('.owl-opreting-system').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        800:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
     }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
+})
 
-
-
-
-
-
-
-
-  function openCity(evt, cityNamee) {
-    var i, tabcontentt, tablinkss;
-    tabcontentt = document.getElementsByClassName("tabcontentt");
-    for (i = 0; i < tabcontentt.length; i++) {
-      tabcontentt[i].style.display = "none";
-    }
-    tablinkss = document.getElementsByClassName("tablinkss");
-    for (i = 0; i < tablinkss.length; i++) {
-      tablinkss[i].className = tablinkss[i].className.replace(" active", "");
-    }
-    document.getElementById(cityNamee).style.display = "flex";
-    evt.currentTarget.className += " active";
-  }
-
-
-
-  $('.your_server-carousel').owlCarousel({
+$('.your_server-carousel').owlCarousel({
     loop:true,
     margin:10,
     responsiveClass:true,
@@ -112,7 +98,7 @@ $('.plan-carousel').owlCarousel({
             nav:true
         },
         600:{
-            items:3,
+            items:2,
             nav:false
         },
         1000:{
@@ -126,17 +112,64 @@ $('.plan-carousel').owlCarousel({
 
 
 
-// Add active class to the current button (highlight it)
-var header = document.getElementById("myDIV");
-var btns = header.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
-  });
-}
 
+
+// Show the first tab and hide the rest
+$('#tabs-nav-one li:first-child').addClass('active');
+$('.tab-content-one').hide();
+$('.tab-content-one:first').show();
+
+// Click function
+$('#tabs-nav-one li').click(function(){
+  $('#tabs-nav-one li').removeClass('active');
+  $(this).addClass('active');
+  $('.tab-content-one').hide();
+  
+  var activeTab = $(this).find('a').attr('href');
+  $(activeTab).fadeIn();
+  return false;
+});
+
+
+
+// Show the first tab and hide the rest
+$('#tabs-nav-two li:first-child').addClass('active');
+$('.tab-content-two').hide();
+$('.tab-content-two:first').show();
+
+// Click function
+$('#tabs-nav-two li').click(function(){
+  $('#tabs-nav-two li').removeClass('active');
+  $(this).addClass('active');
+  $('.tab-content-two').hide();
+  
+  var activeTab = $(this).find('a').attr('href');
+  $(activeTab).fadeIn();
+  return false;
+});
+
+
+
+// Show the first tab and hide the rest
+$('#tabs-nav-three li:first-child').addClass('active');
+$('.tab-content-three').hide();
+$('.tab-content-three:first').show();
+
+// Click function
+$('#tabs-nav-three li').click(function(){
+  $('#tabs-nav-three li').removeClass('active');
+  $(this).addClass('active');
+  $('.tab-content-three').hide();
+  
+  var activeTab = $(this).find('a').attr('href');
+  $(activeTab).fadeIn();
+  return false;
+});
+
+
+
+
+  
 
 // responsive navbar
 
@@ -150,7 +183,36 @@ function toggleNavBar(){
     headerRight.classList.toggle("header-for-mobile");
 }
 
-AOS.init({
-    offset: 300, 
-    duration:2500,
-  });
+
+
+(function($) { 
+    $(function() { 
+  
+      //  open and close nav 
+      $('#navbar-toggle').click(function() {
+        $('nav ul').slideToggle();
+      });
+  
+  
+      // Hamburger toggle
+      $('#navbar-toggle').on('click', function() {
+        this.classList.toggle('active');
+      });
+  
+  
+      // If a link has a dropdown, add sub menu toggle.
+      $('nav ul li a:not(:only-child)').click(function(e) {
+        $(this).siblings('.navbar-dropdown').slideToggle("slow");
+  
+        // Close dropdown when select another dropdown
+        $('.navbar-dropdown').not($(this).siblings()).hide("slow");
+        e.stopPropagation();
+      });
+  
+  
+      // Click outside the dropdown will remove the dropdown class
+      $('html').click(function() {
+        $('.navbar-dropdown').hide();
+      });
+    }); 
+  })(jQuery); 
